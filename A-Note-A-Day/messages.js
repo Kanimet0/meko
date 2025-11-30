6a2e345b52422e8e9abb79ae1dab15433102a9d0
koko <109449180+Kanimet0@users.noreply.github.com>
2025-11-30 19:23:06 +0900

    Add files via upload

---
+++ A-Note-A-Day/messages.js
@@ -0,0 +1,13 @@
+const anniversaryDate = 'September 19, 2025 00:00:00' 
+const noMessage = 'Seems like I forgot to write a message today, or I made a mistake somewhere writing it, either way, still love you. most likely finals aren\'t over yet, so wish you good luck preparing for them.'
+const endMessage = '<br><br><br>Love you♡,<br>Kanimet'
+const messages = {
+    '30:11': 'Hi Meerim, <br> I said we will be having a break, so to make you feel still loved I made this. Every day here will be a new message for you. So, every time you feel like you want to talk to me but you still know we shouldn\'t you can visit this site and read something I wanted to say to you.',
+    '1:12': 'Hi Meerim, <br> Today is Monday, hopefully you understood everything we had on calculus class. If you didn\'t it\'s okay you can always ask GPT or me to help you, I wanted to stop sitting in lounge for too long, but never stop learning :)',
+}
+
+/*
+
+¡™£¢∞§¶•ªº–≠œ∑´®†¥¨ˆøπ“‘«åß∂ƒ©˙∆˚¬…æ≈ç√∫˜µ≤≥÷``
+
+*/
