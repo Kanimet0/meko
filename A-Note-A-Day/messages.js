d9d41b6f4ded14bdf98fad97cf2e3239caf262d7
koko <109449180+Kanimet0@users.noreply.github.com>
2025-12-14 09:26:08 +0900

    Refactor messages and add supportive notes
    Updated messages to shorten noMessage and endMessage. Added new supportive messages for various dates related to finals.

--- A-Note-A-Day/messages.js
+++ A-Note-A-Day/messages.js
@@ -1,9 +1,17 @@
 const anniversaryDate = 'September 19, 2025 00:00:00' 
-const noMessage = 'Seems like I forgot to write a message today, or I made a mistake somewhere writing it, either way, still love you. most likely finals aren\'t over yet, so wish you good luck preparing for them.'
-const endMessage = '<br><br><br>Love you♡,<br>Kanimet'
+const noMessage = 'Seems like I forgot to write a message today, or I made a mistake somewhere writing it, either way, still love you. '
+const endMessage = '<br><br><br>Love you♡<br>Kanimet'
 const messages = {
-    '30:11': 'Hi Meerim, <br> I said we will be having a break, so to make you feel still loved I made this. Every day here will be a new message for you. So, every time you feel like you want to talk to me but you still know we shouldn\'t you can visit this site and read something I wanted to say to you.',
-    '1:12': 'Hi Meerim, <br> Today is Monday, hopefully you understood everything we had on calculus class. If you didn\'t it\'s okay you can always ask GPT or me to help you, I wanted to stop sitting in lounge for too long, but never stop learning :)',
+    '16:12': 'Dear Meerim.  <br>    I know exams are starting and things feel heavy right now. I\'m so proud of how hard you\'ve worked — not because you\'re perfect, but because you keep trying even when you doubt yourself. <br> You don\'t have to fit a mold or be a certain kind of person to be amazing — you already are. <br>I believe in your dreams, your heart, and your journey. I\'m here with you — and even when we\'re far apart, I\'m never truly gone. <br>Take your time to explore and be curious. You don\'t have to have all the answers today — you just have to keep going. <br>  Visit tomorrow, here will be something for you.🤭',
+    '17:12': 'Yaayyy, first final is over, three more to go. Hopefully you wrote it well, and now preparing for biology and calculus exams, or maybe even already wrote it. Either way so proud of you, You are sooo coool. Well it is only the Computer programming so you can rest. Oh and by the way how is the present did you like it? (hope I gave you something). and also don\'t forget to visit me tomorrow also.',
+    '18:12': 'FINALS ARE OVER. <br>Well there is computer programming but man who cares. Congratulations!!! Waiting you tomorrow ;)',
+    '19:12': 'FINALS ARE FINALLY OVER. Even comupter programming should be. So proud of you, I know you wrote them very well so now you can rest. I think we should be having fun with others so hope you are having great time. Do I need to remind you, still waiting you tomorrow.',
+    '20:12': 'You haven\'t left yet so instead of sitting here please go talk to me, I would love to spend more time with you before you leave. I think you get it now.',
+    '21:12': 'Same as yesterday, TALK TO ME BABY. Yeah everyday here will be a new message for you',
+    '22:12': 'Last day before you leave, I hope we are spending a lot of time together, or you are having fun with everyone. You have spent 4 months with us, in two months you will probably miss everyone, so have fun! I did it because tomorrow(Already tomorrow aaaahhh) you are leaving, and we wouldn\'t be able to spend so much time as we did, so it this will be a little reminded that i am alway there with you.',
+    // '23:12': 'FINALS ARE OVER. well there is computer programming but man who cares. Congratulations!!! ',
+    // '24:12': 'FINALS ARE OVER. well there is computer programming but man who cares. Congratulations!!! ',
+    // '25:12': 'FINALS ARE OVER. well there is computer programming but man who cares. Congratulations!!! ',
 }
 
 /*
